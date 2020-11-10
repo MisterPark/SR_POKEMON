dababb5de7377dbf68f0b2b18c4c80650eb087e4
MisterPark <40124101+MisterPark@users.noreply.github.com>
2020-11-10 22:56:00 +0900

    경훈 / NPC 작성중

---
+++ D3DFramework/D3DFramework/NPC.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "GameObject.h"
+class NPC :
+    public GameObject
+{
+public:
+    NPC();
+    virtual ~NPC();
+    // GameObject을(를) 통해 상속됨
+    virtual void Initialize() override;
+    virtual void Release() override;
+    virtual void Update() override;
+    virtual void Render() override;
+
+    virtual void OnCollision(GameObject* target) override;
+
+    void OnTerrain();
+
+
+public:
+    float offsetY = 0.f;
+};
+
