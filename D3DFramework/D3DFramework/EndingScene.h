06d6f3ddbdbf27d3d483aae733385fb9bb9358ac
sneg7777 <71835115+sneg7777@users.noreply.github.com>
2020-11-13 08:57:03 +0900

    Revert "B조 / 최종본 커밋"
    This reverts commit 92998615161607b363cbf043f8c04f4dddde90ca.

--- D3DFramework/D3DFramework/EndingScene.h
+++ D3DFramework/D3DFramework/EndingScene.h
@@ -2,8 +2,6 @@
 #include "IScene.h"
 #include "LobbyWindow.h"
 
-class Label;
-
 class EndingScene :
     public IScene
 {
@@ -13,15 +11,8 @@ public:
     virtual void OnUnloaded() override;
     virtual void Update() override;
 
-
+    LobbyWindow* lobby = nullptr;
 
     static void NextScene();
-
-    Label* label1 = nullptr;
-    Label* label2 = nullptr;
-    Label* label3 = nullptr;
-    Label* label4 = nullptr;
-    Label* label5 = nullptr;
-    Label* label6 = nullptr;
 };
 
